b66310f9fc10e264387b61a9da5dafc3f64625e4
Guilherme Amaro <gui-lidio@hotmail.com>
2021-02-16 01:06:40 +0000

    Prototypes for Player Controller and other effects

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,13 +7,17 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    private Transform cameraHolder;
+    private Transform cameraHolder = null;
+
+    private Rigidbody body;
 
     public float sensitivity = 25.0f;
+    public float speed = 5.0f;
 
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,7 +27,8 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxis("Mouse Y");
 
         //Rotate the camera independently of body for maximum smoothness, just remember to sync the body!
-        cameraHolder.localEulerAngles = cameraHolder.transform.localEulerAngles + Vector3.up * sensitivity * x * Time.deltaTime;
+        if (cameraHolder != null)
+            cameraHolder.localEulerAngles = cameraHolder.transform.localEulerAngles + Vector3.up * sensitivity * x * Time.deltaTime;
     }
 
     void FixedUpdate()
@@ -33,7 +38,17 @@ public class PlayerController : MonoBehaviour
 
         //Apply camera orientation to player orientation, and re-sync camera with object
         //This will mean that a player, like any object in the game, will update at the same fixed rate, so the behavior will be very predictable, while retaining a smooth visual behavior
-        transform.Rotate(transform.up, cameraHolder.localEulerAngles.y);
-        cameraHolder.localEulerAngles = Vector3.zero;
+        if (cameraHolder != null)
+        {
+            transform.Rotate(transform.up, cameraHolder.localEulerAngles.y);
+            cameraHolder.localEulerAngles = Vector3.zero;
+        }
+
+        Vector3 slow = Vector3.Scale(Vector3.one - Vector3.up, -body.velocity);
+        Vector3 move = (transform.right * h + transform.forward * v);
+        move *= speed / Mathf.Max(1f, move.magnitude);
+
+        body.AddForce(move + slow, ForceMode.VelocityChange);
+
     }
 }
